fbd6c4f1319650f0875aec740d6d1e32701f951a
arismendyl <arismendyl@uninorte.edu.co>
2019-12-26 20:57:42 -0500

    Final

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,23 @@
+const initState= {
+    posts: [
+        {id:1, title: 'Squirtle and Egg', body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+        {id:2, title: 'Charmander Laid an Egg', body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+        {id:3, title: 'a Helix Fossil was Found', body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+    ]
+}
+
+const rootReducer = (state=initState, action) =>{
+    if (action.type === 'DELETE_POST') {
+        let newPost = state.posts.filter(post => {
+            return action.id !== post.id
+        })
+        return {
+            ...state,
+            posts: newPost
+        }
+    }
+    return state;
+}
+
+
+export default rootReducer;
